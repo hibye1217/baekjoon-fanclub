e84048950a6be0583b20c41c994d8e9c0e778d58
hibye1217 <mathking2032@gmail.com>
2023-04-08 18:43:18 +0900

    Archived until 080
    081 is ongoing

--- Code/submissions.js
+++ Code/submissions.js
@@ -1,13 +1,15 @@
 const problems = [
-	{ "id": 24912, "index": "A", "title": "카드 색칠" },
-	{ "id": 24913, "index": "B", "title": "개표" },
-	{ "id": 26607, "index": "C", "title": "시로코와 은행털기" },
-	{ "id": 24025, "index": "D", "title": "돌의 정령 줄세우기" },
-	{ "id": 24914, "index": "E", "title": "Split the SSHS" },
-	{ "id": 26615, "index": "F", "title": "다오의 행사 계획하기" },
-	{ "id": 20560, "index": "G", "title": "맛집 탐방" },
-	{ "id": 24026, "index": "H", "title": "기차 여행" },
-	{ "id": 26610, "index": "I", "title": "돌게임과 쿼리" }
+	{ "id": 20944, "index": "A", "title": "팰린드롬 척화비" },
+	{ "id": 16341, "index": "B", "title": "Horsemeet" },
+	{ "id": 14281, "index": "C", "title": "볼록 수열" },
+	{ "id": 1808, "index": "D", "title": "숌작업" },
+	{ "id": 4237, "index": "E", "title": "비 단조성" },
+	{ "id": 18153, "index": "F", "title": "Coloring Contention" },
+	{ "id": 3870, "index": "G", "title": "Find the Multiples" },
+	{ "id": 11307, "index": "H", "title": "String Game" },
+	{ "id": 14368, "index": "I", "title": "Fashion Police (Large)" },
+	{ "id": 10682, "index": "J", "title": "Everlasting -One-" },
+	{ "id": 20234, "index": "K", "title": "Corrupted Sort" }
 ];
 let map = Array(30000);
 problems.forEach(p => { map[p.id] = p.index; });
