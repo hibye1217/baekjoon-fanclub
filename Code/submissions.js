649ce73d267fb07340fff82d2d2361f01c889699
hibye1217 <mathking2032@gmail.com>
2023-05-03 16:07:30 +0900

    Archived

--- Code/submissions.js
+++ Code/submissions.js
@@ -1,17 +1,16 @@
 const problems = [
-	{ "id": 16485, "index": "A", "title": "작도하자! - ②" },
-	{ "id": 16478, "index": "B", "title": "원의 분할" },
-	{ "id": 22290, "index": "C", "title": "lattice" },
-	{ "id": 20206, "index": "D", "title": "푸앙이가 길을 건너간 이유" },
-	{ "id": 1711, "index": "E", "title": "직각삼각형" },
-	{ "id": 9553, "index": "F", "title": "양궁" },
-	{ "id": 2175, "index": "G", "title": "땅 자르기" },
-	{ "id": 16115, "index": "H", "title": "팬이에요" },
-	{ "id": 20032, "index": "I", "title": "Sewing Graph" },
-	{ "id": 1688, "index": "J", "title": "지민이의 테러" },
-	{ "id": 1858, "index": "K", "title": "기울기가 가장 큰 두 점" },
-	{ "id": 22975, "index": "L", "title": "도시 계획" },
-	{ "id": 9374, "index": "M", "title": "감시 카메라" }
+	{ "id": 18742, "index": "A", "title": "Avg" },
+	{ "id": 20772, "index": "B", "title": "Scheduler" },
+	{ "id": 25492, "index": "C", "title": "전깃줄 연결" },
+	{ "id": 21959, "index": "D", "title": "Subway" },
+	{ "id": 16190, "index": "E", "title": "Rising Sun" },
+	{ "id": 27480, "index": "F", "title": "Even Harder" },
+	{ "id": 13873, "index": "G", "title": "Hotel Rewards" },
+	{ "id": 9912, "index": "H", "title": "Lexical" },
+	{ "id": 25559, "index": "I", "title": "패스" },
+	{ "id": 23767, "index": "J", "title": "Access Denied" },
+	{ "id": 25999, "index": "K", "title": "House Numbering" },
+	{ "id": 18735, "index": "L", "title": "Balls" }
 ];
 let map = Array(30000);
 problems.forEach(p => { map[p.id] = p.index; });
