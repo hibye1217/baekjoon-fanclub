ef40aa12782a3b6d44dbe4106b9c716b2a5a8f69
hibye1217 <mathking2032@gmail.com>
2023-12-14 12:27:36 +0900

    SolveCount Ranking

--- ranking.js
+++ ranking.js
@@ -1,4 +1,61 @@
+function findIndex(data, user){
+	for (let i = 0; i < data.length; i++){
+		if (data[i].name == user){ return i; }
+	} return -1;
+}
+function toIndex(problemSet, problem){
+	for (let i = 0; i < problemSet.length; i++){
+		if (problemSet[i].index == problem){ return problemSet[i].id; }
+	}
+	console.error("Problem Not Found", problemSet, problem);
+}
+
 async function main(){
 	const result = await callAPI(0, "https://hibye1217.github.io/baekjoon-fanclub/Data/result.json");
 	
+	let data = [];
+	for (let i = 0; i < result.length; i++){
+		if (result[i].incomplete){ break; }
+		const problemSet = result[i].problems;
+		const submissions = result[i].submissions;
+		
+		for (let j = 0; j < submissions.length; j++){
+			const submission = submissions[j];
+			if (submission.verdict != "AC"){ continue; }
+			if (findIndex(data, submission.handle) == -1){
+				data.push({name: submission.handle, solved: Array()});
+			}
+			let idx = findIndex(data, submission.handle);
+			data[idx].solved[toIndex(problemSet, submission.problem)] = true;
+		}
+	}
+	for (let i = 0; i < data.length; i++){
+		data[i].solveCount = 0;
+		for (let j = 0; j < data[i].solved.length; j++){
+			data[i].solveCount += (data[i].solved[j] ? +1 : +0);
+		}
+	}
+	data.sort((a, b) => (b.solveCount - a.solveCount));
+	
+	const table = document.getElementById('scoreboard-table').children[1];
+	let rank = 0; for (let i = 0; i < data.length; i++){
+		if (i==0 || data[i-1].solveCount != data[i].solveCount){ rank = i+1; }
+		const tr = document.createElement('tr');
+		{ // Rank
+			const td = document.createElement('td');
+			td.innerHTML = rank;
+			tr.appendChild(td);
+		}
+		{ // User
+			const td = document.createElement('td');
+			td.innerHTML = data[i].name;
+			tr.appendChild(td);
+		}
+		{ // SolveCount
+			const td = document.createElement('td');
+			td.innerHTML = data[i].solveCount;
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
 }
