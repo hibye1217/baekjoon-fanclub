28be651db2c70d209e692de207c142155453b2cf
hibye1217 <mathking2032@gmail.com>
2025-11-25 18:04:17 +0900

    #191; 2nd try
    please work please I have my family

--- history.js
+++ history.js
@@ -13,7 +13,6 @@ async function main(){
 			if (result[nxt].order == x+1 && result[nxt].rated){ break; }
 			else{ nxt += 1; }
 		}
-		console.log(x, now, nxt);
 		if (nxt >= len){ break; }
 
 		const title = result[now].title;
@@ -45,7 +44,7 @@ async function main(){
 				if (result[now].version === undefined || result[now].version == 1){
 					if (link === undefined){ td.innerHTML = "\"" + title + "\" 우승"; }
 					else if (link.length == 0){
-						td.innerHTML = "\"" + title + "\" 종료 후 무추첨 당첨"
+						td.innerHTML = "\"" + title + "\" 종료 후 무추첨 당첨";
 					}
 					else{
 						let res = "\"" + title + "\" 종료 후 ";
@@ -70,9 +69,9 @@ async function main(){
 						if (selected[i].type == "Passed"){
 							res += "양도";
 						}
-						res += "</a>"
-						if (i+1 != link.length){ res += " 후 "; }
-					}
+						res += "</a>";
+						if (i+1 != selected.length){ res += " 후 "; }
+					} td.innerHTML = res;
 				}
 			}
 			tr.appendChild(td);
